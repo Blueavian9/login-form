ce411e9ffa4d3a0dea7526c9e8f5d89352db69ad
Cesar Aguilar <Blueavian9@gmail.com>
2024-02-23 15:05:13 -0800

    mvp update

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+// Reference to username/password field contents.
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
+function handleSubmit() {
+  const params = {
+    username: username.value,
+    password: password.value,
+  };
+
+  axios
+    .post("http://localhost:3555/login", params)
+    .then((res) => res.data)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        window.location.href = "dash.html";
+      } else {
+        console.log("no token provided");
+      }
+    });
+}
+
+function handleLogOut() {
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+}
+
+function getDashboard() {
+  let token = localStorage.getItem("token");
+  axios
+    .get("http://localhost:3555/dashboard", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => res.data)
+    .then((data) => {
+      let message = document.querySelector(".message");
+      console.log(data.message);
+      message.innerHTML = data.message;
+    })
+    .catch((err) => (window.location.href = "index.html"));
+}
