942d4837320b02a495bf9364ac943da469e658c2
Cesar Aguilar <Blueavian9@gmail.com>
2024-02-28 14:05:10 -0800

    update commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Reference to username/password field contents.
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
 
 function handleSubmit() {
   const params = {
-    username: username.value,
-    password: password.value,
+    username: usernameInput.value,
+    password: passwordInput.value,
   };
 
   axios
@@ -34,25 +34,48 @@ function getDashboard() {
         Authorization: `Bearer ${token}`,
       },
     })
-    .then((res) => res.data)
-    .then((data) => {
+    .then((res) => {
       let message = document.querySelector(".message");
-      console.log(data.message);
-      message.innerHTML = data.message;
+      console.log(res.data.message);
+      message.innerHTML = res.data.message;
     })
-    .catch((err) => (window.location.href = "index.html"));
+    .catch((err) => {
+      console.error(err);
+      window.location.href = "index.html";
+    });
 }
 
-function handleSignUp() {
-  const params = {
-    username: username.value,
-    password: password.value,
-  };
+/* JS Slideshow Carousel section  */
 
-  axios
-    .post("http://localhost:3555/signup", params)
-    .then((res) => res.data)
-    .then((data) => {
-      console.log(data.message);
-    });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
